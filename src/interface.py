334b72ce208708446f20024171a7d6c412622356
Jumpr15 <chordataosteichthyes@gmail.com>
2026-05-18 23:19:06 +1000

    added hybrid search

--- src/interface.py
+++ src/interface.py
@@ -1,9 +1,13 @@
 import gradio as gr
+import openlit
+
+openlit.init(otlp_endpoint="http://127.0.0.1:4318")
 
 collection_name = "vector-store"
 
 from search.dense_search import DenseSearcher
 from search.sparse_search import SparseSearcher
+from search.hybrid_search import HybridRRFSearcher
 
 def dense_search(text_query):
      searcher = DenseSearcher(collection_name)
@@ -15,11 +19,16 @@ def sparse_search(text_query):
      metadata = searcher.search(text_query)
      return metadata
 
+def hybrid_rrf_search(text_query):
+     searcher = HybridRRFSearcher(collection_name)
+     metadata = searcher.search(text_query)
+     return metadata
+
 def vector_search(text_query):
      return "text"
 
 demo = gr.Interface(
-     fn=sparse_search,
+     fn=hybrid_rrf_search,
      inputs=["text"],
      outputs=["text"],
 )
--- src/search/hybrid_search.py
+++ src/search/hybrid_search.py
@@ -1,18 +1,31 @@
 from qdrant_client import QdrantClient, models
 
-class SparseSearcher:
+class HybridRRFSearcher:
      def __init__(self, collection_name):
+          self.dense_model_name = "sentence-transformers/all-MiniLM-L6-v2"
           self.sparse_model_name = "prithivida/Splade_PP_en_v1"
+          self.dense_vector_name = "dense"
           self.sparse_vector_name = "sparse"
-          
+
           self.collection_name = collection_name
           self.qdrant_client = QdrantClient(url="http://localhost:6333")
           
      def search(self, text: str):
           res = self.qdrant_client.query_points(
                collection_name=self.collection_name,
-               query=models.Document(text=text, model=self.sparse_model_name),
-               using=self.sparse_vector_name,
+               query=models.FusionQuery(
+                    fusion=models.Fusion.RRF
+               ),
+               prefetch=[
+                    models.Prefetch(
+                         query=models.Document(text=text, model=self.dense_model_name),
+                         using=self.dense_vector_name
+                    ),
+                    models.Prefetch(
+                         query=models.Document(text=text, model=self.sparse_model_name),
+                         using=self.sparse_vector_name
+                    )
+               ],
                query_filter=None,
                limit=1
           ).points
@@ -23,6 +36,6 @@ if __name__ == "__main__":
      collection_name = "vector-store"
      query = "skibidi toilet"
      
-     searcher = SparseSearcher(collection_name)
+     searcher = HybridRRFSearcher(collection_name)
      metadata = searcher.search(query)
      print(metadata)
