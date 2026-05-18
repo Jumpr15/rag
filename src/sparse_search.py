ad75b1ff5dbb7b6b204caf75c285a0276c506bd0
Jumpr15 <chordataosteichthyes@gmail.com>
2026-05-18 20:14:37 +1000

    added sparse search

--- src/sparse_search.py
+++ src/sparse_search.py
@@ -1,17 +1,17 @@
 from qdrant_client import QdrantClient, models
 
-class DenseSearcher:
+class SparseSearcher:
      def __init__(self, collection_name):
-          self.dense_model_name = "sentence-transformers/all-MiniLM-L6-v2"
-          self.dense_vector_name = "dense"
+          self.sparse_model_name = "prithivida/Splade_PP_en_v1"
+          self.sparse_vector_name = "sparse"
           self.collection_name = collection_name
           self.qdrant_client = QdrantClient(url="http://localhost:6333")
           
      def search(self, text: str):
           res = self.qdrant_client.query_points(
                collection_name=self.collection_name,
-               query=models.Document(text=text, model=self.dense_model_name),
-               using=self.dense_vector_name,
+               query=models.Document(text=text, model=self.sparse_model_name),
+               using=self.sparse_vector_name,
                query_filter=None,
                limit=1
           ).points
@@ -22,6 +22,6 @@ if __name__ == "__main__":
      collection_name = "vector-store"
      query = "skibidi toilet"
      
-     searcher = DenseSearcher(collection_name)
+     searcher = SparseSearcher(collection_name)
      metadata = searcher.search(query)
      print(metadata)
